222ac6afa7586509dd4feb47e345c5f3f37bcb21
Uryel Farias <uryel.farias@gmail.com>
2024-06-20 23:01:04 -0300

    Selecionando os botões para mudar o background

---
+++ script.js
@@ -0,0 +1,17 @@
+const html = document.querySelector('html');
+const btnFoco = document.querySelector('.app__card-button--foco');
+const btnCurto = document.querySelector('.app__card-button--curto');
+const btnLongo = document.querySelector('.app__card-button--longo');
+
+
+btnFoco.addEventListener('click', () => {
+    html.setAttribute('data-contexto', 'foco');
+})
+
+btnCurto.addEventListener('click', () => {
+    html.setAttribute('data-contexto', 'descanso-curto');
+})
+
+btnLongo.addEventListener('click', () => {
+    html.setAttribute('data-contexto', 'descanso-longo');
+})
