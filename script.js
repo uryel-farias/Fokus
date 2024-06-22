6d9ea4698f4e5947611c19b22f997f07faab9a8a
Uryel Farias <uryel.farias@gmail.com>
2024-06-21 23:38:55 -0300

    selecionando a imagem no DOM

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const html = document.querySelector('html');
 const btnFoco = document.querySelector('.app__card-button--foco');
 const btnCurto = document.querySelector('.app__card-button--curto');
 const btnLongo = document.querySelector('.app__card-button--longo');
-
+const img = document.querySelector('.app__image');
 
 btnFoco.addEventListener('click', () => {
     html.setAttribute('data-contexto', 'foco');
