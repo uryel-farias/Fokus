f9959925d1a47163d8528fd43f52b4e33e97073f
Uryel Farias <uryel.farias@gmail.com>
2024-06-22 22:39:25 -0300

    Mudando as imagens do banner

--- script.js
+++ script.js
@@ -2,16 +2,19 @@ const html = document.querySelector('html');
 const btnFoco = document.querySelector('.app__card-button--foco');
 const btnCurto = document.querySelector('.app__card-button--curto');
 const btnLongo = document.querySelector('.app__card-button--longo');
-const img = document.querySelector('.app__image');
+const banner = document.querySelector('.app__image');
 
 btnFoco.addEventListener('click', () => {
     html.setAttribute('data-contexto', 'foco');
+    banner.setAttribute('src', '/imagens/foco.png')
 })
 
 btnCurto.addEventListener('click', () => {
     html.setAttribute('data-contexto', 'descanso-curto');
+    banner.setAttribute('src', '/imagens/descanso-curto.png');
 })
 
 btnLongo.addEventListener('click', () => {
     html.setAttribute('data-contexto', 'descanso-longo');
+    banner.setAttribute('src', '/imagens/descanso-longo.png');
 })
