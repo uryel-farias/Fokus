0fc73b541a538ec0316ac8793aa23cd8fb095ecd
Uryel Farias <uryel.farias@gmail.com>
2024-06-25 23:26:33 -0300

    Alterando o texto de cada contexto.

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const btnFoco = document.querySelector('.app__card-button--foco');
 const btnCurto = document.querySelector('.app__card-button--curto');
 const btnLongo = document.querySelector('.app__card-button--longo');
 const banner = document.querySelector('.app__image');
+const titulo = document.querySelector('.app__title');
 
 btnFoco.addEventListener('click', () => {
     alterarContexto('foco');
@@ -21,4 +22,26 @@ btnLongo.addEventListener('click', () => {
 function alterarContexto (contexto) {
     html.setAttribute('data-contexto', contexto)
     banner.setAttribute('src', `/imagens/${contexto}.png`)
+    switch (contexto) {
+        case "foco":
+            titulo.innerHTML = `
+                Otimize sua produtividade,<br>
+                <strong class="app__title-strong">mergulhe no que importa.</strong>
+                `
+            break;
+        case "descanso-curto" : 
+            titulo.innerHTML = `
+                Que tal dar uma respirada?<br>
+                <strong class="app__title-strong">Faça uma pausa curta.</strong>
+                `
+            break;
+            case "descanso-longo" : 
+            titulo.innerHTML = `
+                Hora de voltar a superfíce.<br>
+                <strong class="app__title-strong">Faça uma pausa longa.</strong>
+                `
+            default:
+                break;
+
+    }
 }
